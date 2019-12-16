f808b0bfb5ad1e66bfc2ee97dad23ba81e6f17e7
Gian Carlos Cuello Calle <giancarlos.cuello@gmail.com>
2019-12-16 18:44:20 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,3 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyBkpNaAe-khXCowaQnlIhM-7sLfUuWz9Rc",
-    authDomain: "controllerweb-6ee73.firebaseapp.com",
-    databaseURL: "https://controllerweb-6ee73.firebaseio.com",
-    projectId: "controllerweb-6ee73",
-    storageBucket: "",
-    messagingSenderId: "372038451266",
-    appId: "1:372038451266:web:54f6d1b8b9e73977"
-};
+var firebaseConfig = {};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
